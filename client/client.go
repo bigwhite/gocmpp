3faff787d896e2bcc686b6668eb4a0a476214d27
Tony Bai <bigwhite.cn@gmail.com>
2015-10-26 12:59:30 +0800

    add Close method for client

--- client/client.go
+++ client/client.go
@@ -29,33 +29,53 @@ var ErrRespNotMatch = errors.New("The response is not matched with the request")
 // Client stands for one client-side instance, just like a session.
 // It may connect to the server, send & recv cmpp packets and terminate the connection.
 type Client struct {
-	t              uint8 // packet response timeout, default: 60s
-	keepAlive      bool  // indicates whether current session is a keepalive one, default: true
-	conn           net.Conn
-	typ            cmpppacket.Type
-	seqIdGenerator <-chan uint32
+	t         uint8 // packet response timeout, default: 60s
+	keepAlive bool  // indicates whether current session is a keepalive one, default: true
+	conn      net.Conn
+	typ       cmpppacket.Type
+	seqId     <-chan uint32
+	done      chan<- struct{}
 }
 
 // New establishes a new cmpp client.
 func New(typ cmpppacket.Type) *Client {
+	seqId, done := newSeqIdGenerator()
 	return &Client{
-		t:              60,
-		keepAlive:      true,
-		typ:            typ,
-		seqIdGenerator: newSeqIdGenerator(),
+		t:         60,
+		keepAlive: true,
+		typ:       typ,
+		seqId:     seqId,
+		done:      done,
 	}
 }
 
-func newSeqIdGenerator() <-chan uint32 {
-	c := make(chan uint32)
+func newSeqIdGenerator() (<-chan uint32, chan<- struct{}) {
+	out := make(chan uint32)
+	done := make(chan struct{})
+
 	go func() {
 		var i uint32
 		for {
-			c <- i
-			i++
+			select {
+			case out <- i:
+				i++
+			case <-done:
+				close(out)
+				return
+			}
 		}
 	}()
-	return c
+	return out, done
+}
+
+func (cli *Client) Free() {
+	if cli != nil {
+		if cli.conn != nil {
+			cli.conn.Close()
+		}
+		close(cli.done)
+		cli = nil
+	}
 }
 
 // SetT sets the heartbeat response timeout for the client.
@@ -70,6 +90,7 @@ func (cli *Client) SetKeepAlive(keepAlive bool) {
 	cli.keepAlive = keepAlive
 }
 
+// Connect connect to the cmpp server in block mode.
 func (cli *Client) Connect(servAddr, user, password string) error {
 	var err error
 	cli.conn, err = net.Dial("tcp", servAddr)
@@ -107,7 +128,7 @@ func (cli *Client) Connect(servAddr, user, password string) error {
 }
 
 func (cli *Client) SendPacket(packet cmpppacket.Packer) error {
-	data, err := packet.Pack(<-cli.seqIdGenerator)
+	data, err := packet.Pack(<-cli.seqId)
 	if err != nil {
 		return err
 	}
@@ -132,7 +153,6 @@ func (cli *Client) RecvAndUnpackPacket() (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("%x\n", totalLen)
 
 	if cli.typ == cmpppacket.Ver30 {
 		if totalLen < cmpppacket.CMPP3_PACKET_MIN || totalLen > cmpppacket.CMPP3_PACKET_MAX {
@@ -152,19 +172,19 @@ func (cli *Client) RecvAndUnpackPacket() (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("%#v\n", commandId)
 
 	if !((commandId > cmpppacket.CMPP_REQUEST_MIN && commandId < cmpppacket.CMPP_REQUEST_MAX) ||
 		(commandId > cmpppacket.CMPP_RESPONSE_MIN && commandId < cmpppacket.CMPP_RESPONSE_MAX)) {
 		return nil, cmpppacket.ErrCommandIdInvalid
 	}
 
-	// the left packet data
+	// the left packet data (start from seqId in header)
 	var leftData = make([]byte, totalLen-8)
 	_, err = cli.conn.Read(leftData)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(hex.Dump(leftData))
 
 	var p cmpppacket.Packer
 	switch commandId {
@@ -181,7 +201,5 @@ func (cli *Client) RecvAndUnpackPacket() (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("%#v\n", p)
-	fmt.Printf("%#v\n", p.(*cmpppacket.ConnectResponsePacket).Version)
 	return p, nil
 }
