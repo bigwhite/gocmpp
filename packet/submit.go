927f9a62bcdf7930f0b0e4f517eee51113bb5af0
Tony Bai <bigwhite.cn@gmail.com>
2015-11-04 14:27:15 +0800

    implement submit packet pack and unpack

--- packet/submit.go
+++ packet/submit.go
@@ -13,14 +13,367 @@
 
 package cmpppacket
 
-type SubmitOptions struct {
-}
-type cmpp2SubmitPacket struct {
+import "encoding/binary"
+
+// Packet length const for cmpp submit request and response packets.
+const (
+	Cmpp2SubmitReqPktMaxLen uint32 = 12 + 2265  //2277d, 0x8e5
+	Cmpp2SubmitRspPktLen    uint32 = 12 + 8 + 1 //21d, 0x15
+
+	Cmpp3SubmitReqPktMaxLen uint32 = 12 + 3479  //3491d, 0xda3
+	Cmpp3SubmitRspPktLen    uint32 = 12 + 8 + 4 //24d, 0x18
+)
+
+type Cmpp2SubmitReqPkt struct {
+	MsgId              uint64
+	PkTotal            uint8
+	PkNumber           uint8
+	RegisteredDelivery uint8
+	MsgLevel           uint8
+	ServiceId          string
+	FeeUserType        uint8
+	FeeTerminalId      string
+	TpPid              uint8
+	TpUdhi             uint8
+	MsgFmt             uint8
+	MsgSrc             string
+	FeeType            string
+	FeeCode            string
+	ValidTime          string
+	AtTime             string
+	SrcId              string
+	DestUsrTl          uint8
+	DestTerminalId     []string
+	MsgLength          uint8
+	MsgContent         string
+
+	// session info
+	SeqId uint32
 }
 
-type cmpp3SubmitPacket struct {
+type Cmpp2SubmitRspPkt struct {
+	MsgId  uint64
+	Result uint8
+
+	// session info
+	SeqId uint32
 }
 
-func New(t Type) Packer {
-	return nil
+type Cmpp3SubmitReqPkt struct {
+	MsgId              uint64
+	PkTotal            uint8
+	PkNumber           uint8
+	RegisteredDelivery uint8
+	MsgLevel           uint8
+	ServiceId          string
+	FeeUserType        uint8
+	FeeTerminalId      string
+	FeeTerninalType    uint8
+	TpPid              uint8
+	TpUdhi             uint8
+	MsgFmt             uint8
+	MsgSrc             string
+	FeeType            string
+	FeeCode            string
+	ValidTime          string
+	AtTime             string
+	SrcId              string
+	DestUsrTl          uint8
+	DestTerminalId     []string
+	DestTerminalType   uint8
+	MsgLength          uint8
+	MsgContent         string
+	LinkId             string
+
+	// session info
+	SeqId uint32
+}
+
+type Cmpp3SubmitRspPkt struct {
+	MsgId  uint64
+	Result uint32
+
+	// session info
+	SeqId uint32
+}
+
+func (p *Cmpp2SubmitReqPkt) Pack(seqId uint32) ([]byte, error) {
+	var pktLen = uint32(CMPP_HEADER_LEN + 117 + p.DestUsrTl*21 + 1 + p.MsgLength + 8)
+
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_SUBMIT)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+
+	if p.PkTotal == 0 && p.PkNumber == 0 {
+		p.PkTotal, p.PkNumber = 1, 1
+	}
+	w.WriteByte(p.PkTotal)
+	w.WriteByte(p.PkNumber)
+	w.WriteByte(p.RegisteredDelivery)
+	w.WriteByte(p.MsgLevel)
+	w.WriteFixedSizeString(p.ServiceId, 10)
+	w.WriteByte(p.FeeUserType)
+	w.WriteFixedSizeString(p.FeeTerminalId, 21)
+	w.WriteByte(p.TpPid)
+	w.WriteByte(p.TpUdhi)
+	w.WriteByte(p.MsgFmt)
+	w.WriteFixedSizeString(p.MsgSrc, 6)
+	w.WriteString(p.FeeType)
+	w.WriteString(p.FeeCode)
+	w.WriteFixedSizeString(p.ValidTime, 17)
+	w.WriteFixedSizeString(p.AtTime, 17)
+	w.WriteFixedSizeString(p.SrcId, 21)
+	w.WriteByte(p.DestUsrTl)
+
+	for _, d := range p.DestTerminalId {
+		w.WriteFixedSizeString(d, 21)
+	}
+	w.WriteByte(p.MsgLength)
+	w.WriteString(p.MsgContent)
+	w.WriteFixedSizeString("", 8) //Reserved
+
+	return w.Bytes()
+}
+
+func (p *Cmpp2SubmitReqPkt) Unpack(data []byte) error {
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+
+	p.PkTotal = r.ReadByte()
+	p.PkNumber = r.ReadByte()
+	p.RegisteredDelivery = r.ReadByte()
+	p.MsgLevel = r.ReadByte()
+
+	serviceId := r.ReadCString(10)
+	p.ServiceId = string(serviceId)
+
+	p.FeeUserType = r.ReadByte()
+
+	feeTerminalId := r.ReadCString(21)
+	p.FeeTerminalId = string(feeTerminalId)
+
+	p.TpPid = r.ReadByte()
+	p.TpUdhi = r.ReadByte()
+	p.MsgFmt = r.ReadByte()
+
+	msgSrc := r.ReadCString(6)
+	p.MsgSrc = string(msgSrc)
+
+	feeType := make([]byte, 2)
+	r.ReadBytes(feeType)
+	p.FeeType = string(feeType)
+
+	feeCode := r.ReadCString(6)
+	p.FeeCode = string(feeCode)
+
+	validTime := make([]byte, 17)
+	r.ReadBytes(validTime)
+	p.ValidTime = string(validTime)
+
+	atTime := make([]byte, 17)
+	r.ReadBytes(atTime)
+	p.AtTime = string(atTime)
+
+	srcId := r.ReadCString(21)
+	p.SrcId = string(srcId)
+
+	p.DestUsrTl = r.ReadByte()
+
+	for i := 0; i < int(p.DestUsrTl); i++ {
+		destTerminalId := r.ReadCString(21)
+		p.DestTerminalId[i] = string(destTerminalId)
+	}
+
+	p.MsgLength = r.ReadByte()
+
+	msgContent := make([]byte, p.MsgLength)
+	r.ReadBytes(msgContent)
+	p.MsgContent = string(msgContent)
+
+	return r.Error()
+}
+
+func (p *Cmpp2SubmitRspPkt) Pack(seqId uint32) ([]byte, error) {
+	var pktLen = uint32(CMPP_HEADER_LEN + 8 + 1)
+
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_SUBMIT_RESP)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+	w.WriteByte(p.Result)
+
+	return w.Bytes()
+}
+
+func (p *Cmpp2SubmitRspPkt) Unpack(data []byte) error {
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+	p.Result = r.ReadByte()
+
+	return r.Error()
+}
+
+func (p *Cmpp3SubmitReqPkt) Pack(seqId uint32) ([]byte, error) {
+	var pktLen = uint32(CMPP_HEADER_LEN + 129 + p.DestUsrTl*32 + 1 + 1 + p.MsgLength + 20)
+
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_SUBMIT)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+
+	if p.PkTotal == 0 && p.PkNumber == 0 {
+		p.PkTotal, p.PkNumber = 1, 1
+	}
+	w.WriteByte(p.PkTotal)
+	w.WriteByte(p.PkNumber)
+	w.WriteByte(p.RegisteredDelivery)
+	w.WriteByte(p.MsgLevel)
+	w.WriteFixedSizeString(p.ServiceId, 10)
+	w.WriteByte(p.FeeUserType)
+	w.WriteFixedSizeString(p.FeeTerminalId, 32)
+	w.WriteByte(p.FeeTerninalType)
+	w.WriteByte(p.TpPid)
+	w.WriteByte(p.TpUdhi)
+	w.WriteByte(p.MsgFmt)
+	w.WriteFixedSizeString(p.MsgSrc, 6)
+	w.WriteString(p.FeeType)
+	w.WriteString(p.FeeCode)
+	w.WriteFixedSizeString(p.ValidTime, 17)
+	w.WriteFixedSizeString(p.AtTime, 17)
+	w.WriteFixedSizeString(p.SrcId, 21)
+	w.WriteByte(p.DestUsrTl)
+
+	for _, d := range p.DestTerminalId {
+		w.WriteFixedSizeString(d, 32)
+	}
+	w.WriteByte(p.DestTerminalType)
+	w.WriteByte(p.MsgLength)
+	w.WriteString(p.MsgContent)
+	w.WriteFixedSizeString(p.LinkId, 20)
+
+	return w.Bytes()
+}
+
+func (p *Cmpp3SubmitReqPkt) Unpack(data []byte) error {
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+
+	p.PkTotal = r.ReadByte()
+	p.PkNumber = r.ReadByte()
+	p.RegisteredDelivery = r.ReadByte()
+	p.MsgLevel = r.ReadByte()
+
+	serviceId := r.ReadCString(10)
+	p.ServiceId = string(serviceId)
+
+	p.FeeUserType = r.ReadByte()
+
+	feeTerminalId := r.ReadCString(32)
+	p.FeeTerminalId = string(feeTerminalId)
+
+	p.FeeTerninalType = r.ReadByte()
+	p.TpPid = r.ReadByte()
+	p.TpUdhi = r.ReadByte()
+	p.MsgFmt = r.ReadByte()
+
+	msgSrc := r.ReadCString(6)
+	p.MsgSrc = string(msgSrc)
+
+	feeType := make([]byte, 2)
+	r.ReadBytes(feeType)
+	p.FeeType = string(feeType)
+
+	feeCode := r.ReadCString(6)
+	p.FeeCode = string(feeCode)
+
+	validTime := make([]byte, 17)
+	r.ReadBytes(validTime)
+	p.ValidTime = string(validTime)
+
+	atTime := make([]byte, 17)
+	r.ReadBytes(atTime)
+	p.AtTime = string(atTime)
+
+	srcId := r.ReadCString(21)
+	p.SrcId = string(srcId)
+
+	p.DestUsrTl = r.ReadByte()
+
+	for i := 0; i < int(p.DestUsrTl); i++ {
+		destTerminalId := r.ReadCString(32)
+		p.DestTerminalId[i] = string(destTerminalId)
+	}
+
+	p.DestTerminalType = r.ReadByte()
+	p.MsgLength = r.ReadByte()
+
+	msgContent := make([]byte, p.MsgLength)
+	r.ReadBytes(msgContent)
+	p.MsgContent = string(msgContent)
+
+	linkId := r.ReadCString(20)
+	p.LinkId = string(linkId)
+
+	return r.Error()
+}
+
+func (p *Cmpp3SubmitRspPkt) Pack(seqId uint32) ([]byte, error) {
+	var pktLen = uint32(CMPP_HEADER_LEN + 8 + 4)
+
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_SUBMIT_RESP)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+	w.WriteInt(binary.BigEndian, p.Result)
+
+	return w.Bytes()
+}
+
+func (p *Cmpp3SubmitRspPkt) Unpack(data []byte) error {
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+	r.ReadInt(binary.BigEndian, &p.Result)
+
+	return r.Error()
 }
