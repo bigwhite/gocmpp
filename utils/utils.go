d285674cf23dc99c8608765e846694e90bff27dd
Tony Bai <bigwhite.cn@gmail.com>
2015-11-05 10:35:08 +0800

    add vendor, implement utf8 to ucs2 and gb18030 bilateral transforming

--- utils/utils.go
+++ utils/utils.go
@@ -14,9 +14,19 @@
 package cmpputils
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"unicode/utf8"
 	"unsafe"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
+
+var ErrInvalidUtf8Rune = errors.New("Not Invalid Utf8 runes")
 
 func IsBigEndian() bool {
 	var i uint16 = 0x1234
@@ -30,4 +40,70 @@ func IsBigEndian() bool {
 // TimeStamp2Str converts a timestamp(MMDDHHMMSS) int to a string(10 bytes).
 func TimeStamp2Str(t uint32) string {
 	return fmt.Sprintf("%010d", t)
+}
+
+/*
+func Utf8ToUcs2(in string) (string, error) {
+	if !utf8.ValidString(in) {
+		return "", ErrInvalidUtf8Rune
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0, len(in)))
+	for _, r := range in {
+		var cp = uint16(r)
+		err := binary.Write(buf, binary.BigEndian, cp)
+		if err != nil {
+			return "", err
+		}
+	}
+	return string(buf.Bytes()), nil
+}
+*/
+
+func Utf8ToUcs2(in string) (string, error) {
+	if !utf8.ValidString(in) {
+		return "", ErrInvalidUtf8Rune
+	}
+
+	r := bytes.NewReader([]byte(in))
+	t := transform.NewReader(r, unicode.All[1].NewEncoder()) //UTF-16 bigendian, no-bom
+	out, err := ioutil.ReadAll(t)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func Ucs2ToUtf8(in string) (string, error) {
+	r := bytes.NewReader([]byte(in))
+	t := transform.NewReader(r, unicode.All[1].NewDecoder()) //UTF-16 bigendian, no-bom
+	out, err := ioutil.ReadAll(t)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func Utf8ToGB18030(in string) (string, error) {
+	if !utf8.ValidString(in) {
+		return "", ErrInvalidUtf8Rune
+	}
+
+	r := bytes.NewReader([]byte(in))
+	t := transform.NewReader(r, simplifiedchinese.GB18030.NewEncoder())
+	out, err := ioutil.ReadAll(t)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func GB18030ToUtf8(in string) (string, error) {
+	r := bytes.NewReader([]byte(in))
+	t := transform.NewReader(r, simplifiedchinese.GB18030.NewDecoder())
+	out, err := ioutil.ReadAll(t)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -32,3 +32,91 @@ func TestTimeStamp2Str(t *testing.T) {
 		t.Errorf("The result of TimeStamp2Str is %s, not equal to expected: %s\n", s2, "0121080510")
 	}
 }
+
+func TestUtf8ToUcs2(t *testing.T) {
+	// invalid utf8 bytes sequences
+	b1 := []byte{0xe6, 0xb1, 0x89, 0xe6}
+	_, err := cmpputils.Utf8ToUcs2(string(b1))
+	if err == nil {
+		t.Fatal("The in string passed to Utf8ToUcs2 is valid, not equal to our expected")
+	}
+
+	if err != cmpputils.ErrInvalidUtf8Rune {
+		t.Fatalf("The result is %#v, not equal to our expected %#v", err, cmpputils.ErrInvalidUtf8Rune)
+	}
+
+	// valid utf8 bytes sequences
+	b2 := []byte{0xe6, 0xb1, 0x89}
+	s2, err := cmpputils.Utf8ToUcs2(string(b2))
+	if err != nil {
+		t.Fatalf("The error is %#v, not to the result expected: nil", err)
+	}
+
+	sl2 := []byte(s2)
+
+	if sl2[0] != 0x6c {
+		t.Fatalf("The first char is %x, not equal to expected %x\n", sl2[0], 0x6c)
+	}
+
+	if sl2[1] != 0x49 {
+		t.Fatalf("The second char is %x, not equal to expected %x\n", sl2[1], 0x49)
+	}
+}
+
+func TestUcs2ToUtf8(t *testing.T) {
+	u1 := []byte{0x6c, 0x49}
+
+	s1, err := cmpputils.Ucs2ToUtf8(string(u1))
+	if err != nil {
+		t.Fatalf("The error is %#v, not to the result expected: nil", err)
+	}
+
+	if s1 != "汉" {
+		t.Fatalf("The result is %s, not equal to our expected %s", s1, "汉")
+	}
+
+}
+
+func TestUtf8ToGB18030(t *testing.T) {
+	// invalid utf8 bytes sequences
+	b1 := []byte{0xe6, 0xb1, 0x89, 0xe6}
+	_, err := cmpputils.Utf8ToGB18030(string(b1))
+	if err == nil {
+		t.Fatal("The in string passed to Utf8ToGB18030is valid, not equal to our expected")
+	}
+
+	if err != cmpputils.ErrInvalidUtf8Rune {
+		t.Fatalf("The result is %#v, not equal to our expected %#v", err, cmpputils.ErrInvalidUtf8Rune)
+	}
+
+	// valid utf8 bytes sequences
+	b2 := []byte{0xe6, 0xb1, 0x89} //"汉"
+	s2, err := cmpputils.Utf8ToGB18030(string(b2))
+	if err != nil {
+		t.Fatalf("The error is %#v, not to the result expected: nil", err)
+	}
+
+	sl2 := []byte(s2)
+
+	if sl2[0] != 0xba {
+		t.Fatalf("The first char is %x, not equal to expected %x\n", sl2[0], 0xba)
+	}
+
+	if sl2[1] != 0xba {
+		t.Fatalf("The second char is %x, not equal to expected %x\n", sl2[1], 0xba)
+	}
+
+}
+
+func TestGB18030ToUtf8(t *testing.T) {
+	u1 := []byte{0xd6, 0xd0}
+
+	s1, err := cmpputils.GB18030ToUtf8(string(u1))
+	if err != nil {
+		t.Fatalf("The error is %#v, not to the result expected: nil", err)
+	}
+
+	if s1 != "中" {
+		t.Fatalf("The result is %s, not equal to our expected %s", s1, "中")
+	}
+}
