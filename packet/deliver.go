818c1637f11fac59b132a4cc159d5db3b7f4d6b6
Tony Bai <bigwhite.cn@gmail.com>
2015-11-09 16:20:08 +0800

    implement pack and unpack for deliver packet

--- packet/deliver.go
+++ packet/deliver.go
@@ -13,59 +13,287 @@
 
 package cmpppacket
 
-type Cmpp2DeliverReqPkt struct {
+import (
+	"encoding/binary"
+	"errors"
+)
+
+// Packet length const for cmpp deliver request and response packets.
+const (
+	Cmpp2DeliverReqPktMaxLen uint32 = 12 + 233   //245d, 0xf5
+	Cmpp2DeliverRspPktLen    uint32 = 12 + 8 + 1 //21d, 0x15
+
+	Cmpp3DeliverReqPktMaxLen uint32 = 12 + 257   //269d, 0x10d
+	Cmpp3DeliverRspPktLen    uint32 = 12 + 8 + 4 //24d, 0x18
+)
+
+// Errors for result in deliver resp.
+var ErrDeliverInvalidStruct = errors.New("deliver response status: invalid protocol structure")
+var ErrDeliverInvalidCommandId = errors.New("deliver response status: invalid command id")
+var ErrDeliverInvalidSequence = errors.New("deliver response status: invalid message sequence")
+var ErrDeliverInvalidMsgLength = errors.New("deliver response status: invalid message length")
+var ErrDeliverInvalidFeeCode = errors.New("deliver response status: invalid fee code")
+var ErrDeliverExceedMaxMsgLength = errors.New("deliver response status: exceed max message length")
+var ErrDeliverInvalidServiceId = errors.New("deliver response status: invalid service id")
+var ErrDeliverNotPassFlowControl = errors.New("deliver response status: not pass the flow control")
+
+var DeliverRspResultErrMap = map[uint8]error{
+	1: ErrDeliverInvalidStruct,
+	2: ErrDeliverInvalidCommandId,
+	3: ErrDeliverInvalidSequence,
+	4: ErrDeliverInvalidMsgLength,
+	5: ErrDeliverInvalidFeeCode,
+	6: ErrDeliverExceedMaxMsgLength,
+	7: ErrDeliverInvalidServiceId,
+	8: ErrDeliverNotPassFlowControl,
 }
+
+type Cmpp2DeliverReqPkt struct {
+	MsgId            uint64
+	DestId           string
+	ServiceId        string
+	TpPid            uint8
+	TpUdhi           uint8
+	MsgFmt           uint8
+	SrcTerminalId    string
+	RegisterDelivery uint8
+	MsgLength        uint8
+	MsgContent       string
+	Reserve          string
+
+	//session info
+	SeqId uint32
+}
+
 type Cmpp2DeliverRspPkt struct {
+	MsgId  uint64
+	Result uint8
+
+	//session info
+	SeqId uint32
 }
 type Cmpp3DeliverReqPkt struct {
+	MsgId            uint64
+	DestId           string
+	ServiceId        string
+	TpPid            uint8
+	TpUdhi           uint8
+	MsgFmt           uint8
+	SrcTerminalId    string
+	SrcTerminalType  uint8
+	RegisterDelivery uint8
+	MsgLength        uint8
+	MsgContent       string
+	LinkId           string
+
+	//session info
+	SeqId uint32
 }
 type Cmpp3DeliverRspPkt struct {
+	MsgId  uint64
+	Result uint32
+
+	//session info
+	SeqId uint32
 }
 
 // Pack packs the Cmpp2DeliverReqPkt to bytes stream for client side.
 func (p *Cmpp2DeliverReqPkt) Pack(seqId uint32) ([]byte, error) {
-	return nil, nil
+	var pktLen uint32 = CMPP_HEADER_LEN + 65 + uint32(p.MsgLength) + 8
+
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_DELIVER)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+	w.WriteFixedSizeString(p.DestId, 21)
+	w.WriteFixedSizeString(p.ServiceId, 10)
+	w.WriteByte(p.TpPid)
+	w.WriteByte(p.TpUdhi)
+	w.WriteByte(p.MsgFmt)
+	w.WriteFixedSizeString(p.SrcTerminalId, 21)
+	w.WriteByte(p.RegisterDelivery)
+	w.WriteByte(p.MsgLength)
+	w.WriteString(p.MsgContent)
+	w.WriteFixedSizeString(p.Reserve, 8)
+
+	return w.Bytes()
 }
 
 // Unpack unpack the binary byte stream to a Cmpp2DeliverReqPkt variable.
 // After unpack, you will get all value of fields in
 // Cmpp2DeliverReqPkt struct.
 func (p *Cmpp2DeliverReqPkt) Unpack(data []byte) error {
-	return nil
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	// Body
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+
+	destId := r.ReadCString(21)
+	p.DestId = string(destId)
+
+	serviceId := r.ReadCString(10)
+	p.ServiceId = string(serviceId)
+
+	p.TpPid = r.ReadByte()
+	p.TpUdhi = r.ReadByte()
+	p.MsgFmt = r.ReadByte()
+
+	srcTerminalId := r.ReadCString(21)
+	p.SrcTerminalId = string(srcTerminalId)
+
+	p.RegisterDelivery = r.ReadByte()
+	p.MsgLength = r.ReadByte()
+
+	msgContent := make([]byte, p.MsgLength)
+	r.ReadBytes(msgContent)
+	p.MsgContent = string(msgContent)
+
+	reserve := r.ReadCString(8)
+	p.Reserve = string(reserve)
+
+	return r.Error()
 }
 
 // Pack packs the Cmpp2DeliverRspPkt to bytes stream for client side.
 func (p *Cmpp2DeliverRspPkt) Pack(seqId uint32) ([]byte, error) {
-	return nil, nil
+	var pktLen uint32 = Cmpp2DeliverRspPktLen
+
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_DELIVER_RESP)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+	w.WriteByte(p.Result)
+
+	return w.Bytes()
 }
 
 // Unpack unpack the binary byte stream to a Cmpp2DeliverRspPkt variable.
 // After unpack, you will get all value of fields in
 // Cmpp2DeliverRspPkt struct.
 func (p *Cmpp2DeliverRspPkt) Unpack(data []byte) error {
-	return nil
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+	p.Result = r.ReadByte()
+
+	return r.Error()
 }
 
 // Pack packs the Cmpp3DeliverReqPkt to bytes stream for client side.
 func (p *Cmpp3DeliverReqPkt) Pack(seqId uint32) ([]byte, error) {
-	return nil, nil
+	var pktLen uint32 = CMPP_HEADER_LEN + 77 + uint32(p.MsgLength) + 20
+
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_DELIVER)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+	w.WriteFixedSizeString(p.DestId, 21)
+	w.WriteFixedSizeString(p.ServiceId, 10)
+	w.WriteByte(p.TpPid)
+	w.WriteByte(p.TpUdhi)
+	w.WriteByte(p.MsgFmt)
+	w.WriteFixedSizeString(p.SrcTerminalId, 32)
+	w.WriteByte(p.SrcTerminalType)
+	w.WriteByte(p.RegisterDelivery)
+	w.WriteByte(p.MsgLength)
+	w.WriteString(p.MsgContent)
+	w.WriteFixedSizeString(p.LinkId, 20)
+
+	return w.Bytes()
 }
 
 // Unpack unpack the binary byte stream to a Cmpp3DeliverReqPkt variable.
 // After unpack, you will get all value of fields in
 // Cmpp3DeliverReqPkt struct.
 func (p *Cmpp3DeliverReqPkt) Unpack(data []byte) error {
-	return nil
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	// Body
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+
+	destId := r.ReadCString(21)
+	p.DestId = string(destId)
+
+	serviceId := r.ReadCString(10)
+	p.ServiceId = string(serviceId)
+
+	p.TpPid = r.ReadByte()
+	p.TpUdhi = r.ReadByte()
+	p.MsgFmt = r.ReadByte()
+
+	srcTerminalId := r.ReadCString(21)
+	p.SrcTerminalId = string(srcTerminalId)
+	p.SrcTerminalType = r.ReadByte()
+
+	p.RegisterDelivery = r.ReadByte()
+	p.MsgLength = r.ReadByte()
+
+	msgContent := make([]byte, p.MsgLength)
+	r.ReadBytes(msgContent)
+	p.MsgContent = string(msgContent)
+
+	linkId := r.ReadCString(20)
+	p.LinkId = string(linkId)
+
+	return r.Error()
 }
 
 // Pack packs the Cmpp3DeliverRspPkt to bytes stream for client side.
 func (p *Cmpp3DeliverRspPkt) Pack(seqId uint32) ([]byte, error) {
-	return nil, nil
+	var pktLen uint32 = Cmpp3DeliverRspPktLen
+	var w = newPacketWriter(pktLen)
+
+	// Pack header
+	w.WriteInt(binary.BigEndian, pktLen)
+	w.WriteInt(binary.BigEndian, CMPP_DELIVER_RESP)
+	w.WriteInt(binary.BigEndian, seqId)
+	p.SeqId = seqId
+
+	// Pack Body
+	w.WriteInt(binary.BigEndian, p.MsgId)
+	w.WriteInt(binary.BigEndian, p.Result)
+
+	return w.Bytes()
 }
 
 // Unpack unpack the binary byte stream to a Cmpp3DeliverRspPkt variable.
 // After unpack, you will get all value of fields in
 // Cmpp3DeliverRspPkt struct.
 func (p *Cmpp3DeliverRspPkt) Unpack(data []byte) error {
-	return nil
+	var r = newPacketReader(data)
+
+	// Sequence Id
+	r.ReadInt(binary.BigEndian, &p.SeqId)
+
+	r.ReadInt(binary.BigEndian, &p.MsgId)
+	r.ReadInt(binary.BigEndian, &p.Result)
+
+	return r.Error()
 }
