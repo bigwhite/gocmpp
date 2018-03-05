0aa97386d078667d4083e7cb8d358a6e942cac7e
Tony Bai <baim@neusoft.com>
2018-03-05 14:43:36 +0800

    explictly use unicode.UTF16 instead unicode.All for the order change when unicode evolution

--- utils/utils.go
+++ utils/utils.go
@@ -48,7 +48,7 @@ func Utf8ToUcs2(in string) (string, error) {
 	}
 
 	r := bytes.NewReader([]byte(in))
-	t := transform.NewReader(r, unicode.All[1].NewEncoder()) //UTF-16 bigendian, no-bom
+	t := transform.NewReader(r, unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewEncoder()) //UTF-16 bigendian, no-bom
 	out, err := ioutil.ReadAll(t)
 	if err != nil {
 		return "", err
@@ -58,7 +58,7 @@ func Utf8ToUcs2(in string) (string, error) {
 
 func Ucs2ToUtf8(in string) (string, error) {
 	r := bytes.NewReader([]byte(in))
-	t := transform.NewReader(r, unicode.All[1].NewDecoder()) //UTF-16 bigendian, no-bom
+	t := transform.NewReader(r, unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewDecoder()) //UTF-16 bigendian, no-bom
 	out, err := ioutil.ReadAll(t)
 	if err != nil {
 		return "", err
